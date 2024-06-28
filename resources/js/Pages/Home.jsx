c5092d2239886b970cd5d30cd7700f10ddcbeac0
Martin Richter <karag2006@gmx.net>
2024-06-28 18:53:27 +0200

    init

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,6 @@
+import ChatLayout from "@/Layouts/ChatLayout";
+import { Head } from "@inertiajs/react";
+
+export default function Dashboard({ auth }) {
+    return <ChatLayout>Messages</ChatLayout>;
+}
